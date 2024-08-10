3cbc84442d8022c1ec809e9a564ae33ed58f8fa7
vikas-851 <167636013+vikas-851@users.noreply.github.com>
2024-08-10 23:07:03 +0530

    Update welcome.py

--- HINATAMUSIC/plugins/tools/welcome.py
+++ HINATAMUSIC/plugins/tools/welcome.py
@@ -120,7 +120,7 @@ async def greet_new_member(_, member: ChatMemberUpdated):
                 user.photo.big_file_id, file_name=f"pp{user.id}.png"
             )
         except AttributeError:
-            pic = "ANNIEMUSIC/assets/upic.png"
+            pic = "HINATAMUSIC/assets/upic.png"
         if (temp.MELCOW).get(f"welcome-{member.chat.id}") is not None:
             try:
                 await temp.MELCOW[f"welcome-{member.chat.id}"].delete()
