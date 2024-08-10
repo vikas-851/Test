ea967608a1dd2f15941dc17209bd11296a6b3401
vikas-851 <167636013+vikas-851@users.noreply.github.com>
2024-08-10 20:28:39 +0530

    Add files via upload

---
+++ HINATAMUSIC/__init__.py
@@ -0,0 +1,29 @@
+from HINATAMUSIC.core.bot import JARVIS
+from HINATAMUSIC.core.dir import dirr
+from HINATAMUSIC.core.git import git
+from HINATAMUSIC.core.userbot import Userbot
+from HINATAMUSIC.misc import dbb, heroku
+
+from SafoneAPI import SafoneAPI
+from .logging import LOGGER
+
+dirr()
+git()
+dbb()
+heroku()
+
+app = JARVIS()
+api = SafoneAPI()
+userbot = Userbot()
+
+
+from .platforms import *
+
+Apple = AppleAPI()
+Carbon = CarbonAPI()
+SoundCloud = SoundAPI()
+Spotify = SpotifyAPI()
+Resso = RessoAPI()
+Telegram = TeleAPI()
+YouTube = YouTubeAPI()
+YTB = YTM()
